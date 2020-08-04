587beeb70f1fa3548e883c6d970188faa6805509
Lucas Marques <lucasmarques73@hotmail.com>
2020-08-04 14:09:53 -0300

    Brincando com fast deep equal

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const equal = require("fast-deep-equal")
+const equal = require("fast-deep-equal");
 
 const cameras = [
-    {
-        id   : "456",
-        label: "camera2"
-    },
-    {
-        id   : "123",
-        label: "camera1"
-    }    
-]
+  {
+    id: "456",
+    label: "camera2",
+  },
+  {
+    id: "123",
+    label: "camera1",
+  },
+];
 const microfones = [
-    {
-        id   : "789",
-        label: "microfone1"
-    },
-    {
-        id   : "012",
-        label: "microfone2"
-    }
-]
+  {
+    id: "789",
+    label: "microfone1",
+  },
+  {
+    id: "012",
+    label: "mi crofone2",
+  },
+];
 
-const camerasDiferente = cameras.push({ id: "nova", label: "novaCamera" })
+const camerasDiferente = cameras.push({ id: "nova", label: "novaCamera" });
 
 const arr1 = { videoDevices: cameras, audioDevices: microfones };
 const arrEqual1 = { videoDevices: cameras, audioDevices: microfones };
 const arr2 = { videoDevices: camerasDiferente, audioDevices: microfones };
 
-console.log("is equal", equal(arr1, arrEqual1))
-console.log("is equal", equal(arr1, arr2))
+console.log("is equal", equal(arr1, arrEqual1));
+console.log("is equal", equal(arr1, arr2));
